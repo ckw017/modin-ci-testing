430febb7ed6f4f591e97b42a6564eb144524b686
Chris Wong <chriskw.xyz@gmail.com>
2021-06-04 09:27:20 -0700

    Run through multiple versions of ray

--- modin/pandas/test/conftest.py
+++ modin/pandas/test/conftest.py
@@ -11,6 +11,16 @@ def pytest_sessionstart(session):
     if TestRayClient.get():
         import modin.pandas
         import ray
+        from packaging import version
+        if version.parse(ray.__version__) <= version.parse("1.3.0"):
+            from ray.util.client.common import ClientBaseRef, ClientObjectRef
+            # This part fixes an issue in ray 1.3 that will be resolved in the 1.4
+            # release (https://github.com/ray-project/ray/pull/15320)
+            # Can be removed once the ray version for the ray client tests is bumped
+            # to 1.4
+            def patched_eq(self, other):
+                return isinstance(other, ClientBaseRef) and self.id == other.id
+            ClientObjectRef.__eq__ = patched_eq
 
         port = '50051'
         # Clean up any extra processes from previous runs
